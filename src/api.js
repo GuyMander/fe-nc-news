7496bbfddc8f6469f660525c35cb0babbcfb4480
GuyMander <cwguymander@hotmail.com>
2024-03-14 11:49:46 +0000

    completed optimistic vote rendering

--- src/api.js
+++ src/api.js
@@ -4,19 +4,25 @@ const baseURL = "https://nc-news-project-qeuz.onrender.com/api";
 const nc_news_API = axios.create({baseURL});
 
 export async function getArticles(){ //array of articles
-    const response = await nc_news_API.get('/articles')
-    const articles = response.data.articles
-    return articles
+    const response = await nc_news_API.get('/articles');
+    const articles = response.data.articles;
+    return articles;
 }
 
 export async function getArticleById(id){
-    const response = await nc_news_API.get(`/articles/${id}`)
-    const article = response.data.article
-    return article
+    const response = await nc_news_API.get(`/articles/${id}`);
+    const article = response.data.article;
+    return article;
 }
 
 export async function getCommentsById(id){
-    const response = await nc_news_API.get(`/articles/${id}/comments`)
-    const comments = response.data.comments
+    const response = await nc_news_API.get(`/articles/${id}/comments`);
+    const comments = response.data.comments;
     return comments;
+}
+
+export async function patchVotesByArticleId(id, voteObj){
+    const response = await nc_news_API.patch(`/articles/${id}`, voteObj);
+    const updated_article = response.data.updated_article;
+    return updated_article;
 }
